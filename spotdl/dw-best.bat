85443ab4e3b9b02f3cb8563c111d341ce6b6dc01
herrlamatv <mail@survivalarmy.de>
2026-09-12 03:46:49 +0200

    spotdl (will write the readme tomorrow/another time)

---
+++ spotdl/dw-best.bat
@@ -0,0 +1,26 @@
+@echo off
+title spotdl dw (best)
+
+:: saving path (for the downloaded songs)
+:: Example: "SAVE_DIR=C:\Users\YOURUSERNAME\Music\Downloads"
+::set "SAVE_DIR=C:\Users\<user>\Desktop\spotdl"
+set "SAVE_DIR=%USERPROFILE%\Desktop\spotdl"
+
+if not exist "%SAVE_DIR%" mkdir "%SAVE_DIR%"
+cd /d "%SAVE_DIR%"
+
+echo Simple Edition (Best / original)
+echo Music dw @ %SAVE_DIR%
+echo Enter a link (song/album/playlist/artist):
+set /p URL=
+
+echo.
+echo Starting Download...
+echo.
+
+spotdl download "%URL%" --format m4a --bitrate disable --threads 4 --output "{artists} - {title}.{output-ext}"
+
+echo.
+echo Done.
+echo You can close this tab
+pause
